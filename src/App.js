fdf31201c671dcbcfa5077fbddaf8e5a65886a78
Vadim Rikov <rikovfront@gmail.com>
2022-01-28 17:26:57 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Counter />
+  </div>
+);
+
+export default App;
